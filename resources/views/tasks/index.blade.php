f63445fd32176c5d821355372a4ca403ed4044b0
Kestas <kestutisjankevicius9@gmail.com>
2023-11-07 13:43:22 +0200

    Button

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,7 +8,38 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                Coming soon.
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <x-link href="{{ route('tasks.create') }}" class="m-4">Add new task</x-link>
+                    <table class="w-full text-sm text-left text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Task Name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                             @forelse ($tasks as $task) 
+                                <tr class="bg-white border-b">
+                                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                        {{ $task->name }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="{{ route('tasks.edit', $task) }}" class="font-medium text-blue-600 hover:underline">Edit</a>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr class="bg-white border-b">
+                                    <td colspan="2" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                        {{ __('No tasks found') }}
+                                    </td>
+                                </tr>
+                             @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
